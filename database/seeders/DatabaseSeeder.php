54c7690ca09b9d3559b81812f25f57a031cc3c64
Safaa Duridi <mghanem@360moms.net>
2025-01-28 02:33:32 +0200

    DB Seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'phone' => '123456789',
+            'address' => 'address',
+            'city' => 'city',
+            'password' => Hash::make('123123123'),
+            'is_admin' => true
         ]);
     }
 }
